46637fc3ec3e4a58ba0e77d9165985bc7e64cc42
Bücherl Manuel <manuel.buecherl@web.de>
2021-04-08 01:45:48 +0200

    V1.0

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ from PyQt6 import uic
 # Variablen
 
 Planes = []
+InfoTexte = []
+
+NullpunktLBL = [" LBL ", " CALL LBL 50", " CYCL DEF 7.0 NULLPUNKT", " CYCL DEF 7.1  X+0", " CYCL DEF 7.1  Y+0", " CYCL DEF 7.1  Z+0", " LBL 0"]
 
 # Funktionen
 
@@ -35,33 +38,53 @@ def addNumbers(text):
             text[i] = "0" + text[i]
     return text
 
-def addLBL(text, Planes):
+def addLBL(text, Planes, InfoTexte):
     for i in range(len(Planes)):
         for x in range(len(text)):
             s=0
-            print(x)
             if str(Planes[i]) == str(text[x-s]):
                 LabelNummer = 50 + i
                 text.insert(x+1," CALL LBL " + str(LabelNummer) + "\n")
                 s=s+1
 
+    for i in range(len(Planes)):
+        number = 50 + i
+        text.insert(len(text)-1, NullpunktLBL[0] + str(number) + " ; " + InfoTexte[i])
+        text.insert(len(text)-1, NullpunktLBL[1] + "\n")
+        text.insert(len(text)-1, NullpunktLBL[2] + "\n")
+        text.insert(len(text)-1, NullpunktLBL[3] + "\n")
+        text.insert(len(text)-1, NullpunktLBL[4] + "\n")
+        text.insert(len(text)-1, NullpunktLBL[5] + "\n")
+        text.insert(len(text)-1, NullpunktLBL[6] + "\n")
+        text.insert(len(text)-1, " ;" + "\n")
 
     return text
 
-def findPlanes(text, Planes):
+def findPlanes(text):
+    planesVar = []
+    infotext = []
+    
     for i in range(len(text)):
         if search("PLANE", text[i]):
-            Planes.insert(len(Planes), text[i])
+            planesVar.insert(len(planesVar), text[i])
+            
+            for x in range(5):
+                if search("SPATIAL", text[i]):
+                    if search("Operation", text[i+x]):
+                        infotext.insert(len(infotext), text[i+x+1])
+                    if search("Operation", text[i-x]):
+                        infotext.insert(len(infotext), text[i-x+1])
+
     PlanesZwischenspeicher = []
-    for i in Planes:
+    for i in planesVar:
         if i not in PlanesZwischenspeicher:
             PlanesZwischenspeicher.append(i)
-    Planes = PlanesZwischenspeicher
+    planesVar = PlanesZwischenspeicher
 
-    for i in range(len(Planes)):
-        if search("PLANE RESET TURN", Planes[i]):
-            Planes.pop(i)
-    return Planes      
+    for i in range(len(planesVar)):
+        if search("PLANE RESET TURN", planesVar[i]):
+            planesVar.pop(i)
+    return planesVar, infotext   
 
 def addEND(text):
     Endcode = open("Endcode.h", 'r')
@@ -101,17 +124,20 @@ def ButtonStartEditFile():
     ProgressBar(20)
     Datei.close()
 
-    # text = removeNumbers(text)
+    text = removeNumbers(text)
     ProgressBar(30)
-    # Planes = findPlanes(text, Planes)
+    text = addEND(text)
     ProgressBar(40)
-    # text = addLBL(text, Planes)
+    Planes = findPlanes(text)
+    InfoTexte = Planes[1]
+    InfoTexte.insert(0, "Nullpunkt zurücksetzen")
+    Planes = Planes[0]
     ProgressBar(50)
-    # text = addEND(text)
+    text = addLBL(text, Planes, InfoTexte)
     ProgressBar(60)
-    # text = addNumbers(text)
+    text = addNumbers(text)
     ProgressBar(70)
-    # DateiSchreiben(DateiPfad, text)
+    DateiSchreiben(DateiPfad, text)
     ProgressBar(100)
 
     w.LabelDone.setVisible(True)

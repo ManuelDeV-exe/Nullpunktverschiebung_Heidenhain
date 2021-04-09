00c2793c751d0cee4e60f04abeba4a349cb81894
Bücherl Manuel <manuel.buecherl@web.de>
2021-04-09 02:00:58 +0200

    V1.1

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from cx_Freeze import setup, Executable
 # Dateien 
 files = ['Gui/', 'Endcode.h']
 
+# Build Optionen
+build_exe_options = {'include_files' : files}
+
 # Ziel
 target = Executable(
     script="main.py",
@@ -18,6 +21,6 @@ setup(
     version = "1.1",
     description = "Nullpunkt Bscheißer für Heidenhain Programme",
     author= "Manuel Bücherl",
-    options = {'build_exe' : {'include_files' : files}},
+    options = {'build_exe' : build_exe_options},
     executables = [target]
     )
